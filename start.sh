66209bcfb2b6e5fae8284b935c1f0cde16e951bc
mahdi <33233151+mahdi-13@users.noreply.github.com>
2026-04-30 00:18:32 +0330

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# جایگزینی پورت 8080 در کانفیگ با پورتی که Railway اختصاص داده است
-sed -i "s/8080/$PORT/g" /etc/xray/config.json
-
-# اجرای Xray
-/usr/local/bin/xray run -c /etc/xray/config.json
